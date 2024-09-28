a054cd90106d8b92c547f15d91023844f9283535
Lucian I. Last <li@last.nl>
2024-09-28 14:36:42 +0200

    Add icon and promotion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import Index from "./pages";
 import Register from "./pages/register";
 import Private from "./pages/private";
 import Login from "./pages/login";
+import { Avatar, Button, Link } from "@nextui-org/react";
+import { StarIcon } from "lucide-react";
 
 function App() {
   const router = createBrowserRouter([
@@ -26,6 +28,29 @@ function App() {
 
   return (
     <div className="bg-background text-foreground">
+      <div className="w-full flex flex-row-reverse items-center gap-4 p-4 absolute top-0">
+        <Button
+          as={Link}
+          target="_blank"
+          color="primary"
+          href="https://github.com/stupidwebauthn/example"
+        >
+          <StarIcon className="me-1 fill-yellow-400" strokeWidth={0} />
+          Github
+        </Button>
+        <Button
+          as={Link}
+          target="_blank"
+          variant="flat"
+          href="https://github.com/lil5"
+        >
+          <Avatar
+            className="w-6 h-6"
+            src="https://avatars.githubusercontent.com/u/17646836?v=4"
+          ></Avatar>
+          Lucian Last
+        </Button>
+      </div>
       <RouterProvider router={router} />
     </div>
   );
